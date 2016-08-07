2d831d903f386884236572fdef261bca34ea819d
Giovanny González <Giovanny Gonzlez>
2016-08-07 14:43:40 -0500

    Se crea la activity Boot para iniciar el juego, se crea la activity Inicio, mostrando las opciones del juego, se crea la activity About, que se accedesde el boton Acerca de

---
+++ app/src/main/java/com/giovanny/asteroides/About.java
@@ -0,0 +1,15 @@
+package com.giovanny.asteroides;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+/**
+ * Created by giovannyg on 05/08/2016.
+ */
+public class About extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
--- app/src/main/java/com/giovanny/asteroides/Inicio.java
+++ app/src/main/java/com/giovanny/asteroides/Inicio.java
@@ -1,15 +1,22 @@
 package com.giovanny.asteroides;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.R.*;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
-public class Inicio extends AppCompatActivity {
+public class Inicio extends Activity implements View.OnClickListener {
+
+    private Button btn_jugar, btn_configurar, btn_about, btn_salir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,36 @@ public class Inicio extends AppCompatActivity {
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             Window w = getWindow();
             w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+            iniciarVistas();
+        }
+    }
+
+ /*   public boolean onKeyDown(int keyCode, KeyEvent e){
+        if(keyCode == KeyEvent.KEYCODE_BACK) {
+            finish();
+        }
+        return super.onKeyDown(keyCode, e);
+    }*/
+
+    public void iniciarVistas(){
+        btn_about = (Button) findViewById(R.id.about);
+
+        btn_about.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v.getTag() != null)
+
+        switch((String)v.getTag()) {
+            case "about":
+                startActivity(new Intent(this, About.class));
+                break;
+            case  "salir":
+                finish();
+                break;
         }
     }
 }
