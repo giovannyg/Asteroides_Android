21dee66b59340cd9d2bacfe366958a96e7228121
Giovanny Gonzalez <giovanny.gop@gmail.com>
2016-08-18 17:08:01 -0500

    Se crea la actividad principal Juego, se crea una actividad VistaJuego que será la vista del juego, se crea la clase Grafico que dibujará el grafico y manejara movimientos, rotaciones y colisiones del grafico

---
+++ app/src/main/java/com/giovanny/asteroides/Juego.java
@@ -0,0 +1,16 @@
+package com.giovanny.asteroides;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+/**
+ * Created by giovannyg on 18/08/2016.
+ */
+public class Juego extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_juego);
+    }
+}
---
+++ app/src/main/java/com/giovanny/asteroides/VistaJuego.java
@@ -0,0 +1,13 @@
+package com.giovanny.asteroides;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by giovannyg on 18/08/2016.
+ */
+public class VistaJuego extends View {
+    public VistaJuego(Context context) {
+        super(context);
+    }
+}
